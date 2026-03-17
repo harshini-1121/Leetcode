f5c7294f3e30b42c4d1a17b00ce8337b1cb3a84a
harshini-1121 <71762333015@cit.edu.in>
2026-03-17 12:26:10 +0530

    Time: 6 ms (46.61%), Space: 19.2 MB (82.62%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        r_to_int ={'I':1 ,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+
+        res = 0
+        for i in range(len(s)):
+            if i < len(s) -1 and r_to_int[s[i]] < r_to_int[s[i+1]]:
+                res -= r_to_int[s[i]]
+            else:
+                res += r_to_int[s[i]]
+        return res
